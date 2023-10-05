7385f8d2cba9f4666f8886579100e452774c447d
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-05 08:19:43 -0400

    Initial Commit

---
+++ ArraysTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ArraysTest
+//
+//  Created by Sergey Lukaschuk on 2023-10-05.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ArraysTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ArraysTest
+//
+//  Created by Sergey Lukaschuk on 2023-10-05.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
